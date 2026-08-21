f9fca2eccb624db0ef6cb40fa58eeb822f8b2ff6
Yudhishthir-reva <yudhishthir@revateam.com>
2026-08-21 10:22:37 +0530

    Initial Commit

---
+++ TrudataRider/TrudataRiderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrudataRiderApp.swift
+//  TrudataRider
+//
+//  Created by Reva on 21/08/26.
+//
+
+import SwiftUI
+
+@main
+struct TrudataRiderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
